bc331364055fc499a9994920e5695d94e2568ca1
Nguyen Van Quoc <57782951+quocnv2@users.noreply.github.com>
2022-11-21 23:55:38 +0700

    day 1

---
+++ bai2/ex1_string.py
@@ -0,0 +1,21 @@
+# bai 1
+name = "Python basic NTQ"
+
+print((name[0] + name[-1]))
+# bai2
+
+print(name[7:])
+
+print(name[1:14])
+
+print(name[::-1])
+print(name[1] + name[3] + name[5] + name[7] + name[9] + name[11] + name[13] + name[15])
+
+# bai 3
+print(name[0] + '-' + name[7] + '-' + name[13])
+print(name[-3] + name[-5].upper() + name[-1])
+# bai4
+
+new_str = name.replace(" ", "")
+
+print(new_str)
