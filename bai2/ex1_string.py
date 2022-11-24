327d7b2104ce28477959891e24d8e034787cfd28
Nguyen Van Quoc <quoc.nguyen@ntq-solution.com.vn>
2022-11-24 22:52:03 +0700

    commit day 4

--- bai2/ex1_string.py
+++ bai2/ex1_string.py
@@ -2,20 +2,23 @@
 name = "Python basic NTQ"
 
 print((name[0] + name[-1]))
-# bai2
-
+# # bai2
+#
 print(name[7:])
-
+#
 print(name[1:14])
-
+#
 print(name[::-1])
-print(name[1] + name[3] + name[5] + name[7] + name[9] + name[11] + name[13] + name[15])
 
-# bai 3
+print(name[1::2])
+
+# # bai 3
 print(name[0] + '-' + name[7] + '-' + name[13])
 print(name[-3] + name[-5].upper() + name[-1])
-# bai4
-
+# # bai4
+#
 new_str = name.replace(" ", "")
-
+#
 print(new_str)
+
+#su dung slice
